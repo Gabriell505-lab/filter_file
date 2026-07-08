907771e83ef427131136a3eacdc00aacb1b61414
Gabs <gabriellsantos860@gmail.com>
2026-07-08 20:13:09 -0300

    corrigir alguns erros

--- buscadorDePastas.py
+++ buscadorDePastas.py
@@ -1,18 +1,17 @@
 import os
 
-# Pergunta o nome da pasta
+
 nome = input("Digite o nome da pasta: ")
 
-# Pasta onde a busca vai começar
+
 pasta_inicial = "C:\\Users"
 
-# Variável para saber se encontrou ou não
+
 encontrou = False
 
-# Percorre todas as pastas
 for caminho, pastas, arquivos in os.walk(pasta_inicial):
 
-    # Verifica se existe uma pasta com esse nome (ignorando maiúsculas/minúsculas)
+    
     for pasta in pastas:
         if pasta.lower() == nome.lower():
             print("Pasta encontrada!")
@@ -20,6 +19,6 @@ for caminho, pastas, arquivos in os.walk(pasta_inicial):
             print("-" * 40)
             encontrou = True
 
-# Se não encontrou nenhuma pasta
+
 if not encontrou:
     print("Pasta não encontrada.")
