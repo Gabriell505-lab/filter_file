488c01e015da528f09e2a824f3071ab80c04360f
Gabs <gabriellsantos860@gmail.com>
2026-07-08 21:46:47 -0300

    Contador

--- buscadorDePastas.py
+++ buscadorDePastas.py
@@ -8,17 +8,26 @@ pasta_inicial = "C:\\Users"
 
 
 encontrou = False
+contador = 0
+
 
 for caminho, pastas, arquivos in os.walk(pasta_inicial):
 
     
     for pasta in pastas:
+
+        
         if pasta.lower() == nome.lower():
-            print("Pasta encontrada!")
-            print(os.path.join(caminho, pasta))
-            print("-" * 40)
+
+            contador += 1
             encontrou = True
 
+            print(f"\nResultado {contador}:")
+            print(os.path.join(caminho, pasta))
+            print("-" * 40)
 
-if not encontrou:
-    print("Pasta não encontrada.")
+
+if encontrou:
+    print(f"\nForam encontradas {contador} pasta(s).")
+else:
+    print("\nPasta não encontrada.")
